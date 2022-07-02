cc60ea0d3cd0b4ac79f33c3ebf72ddabd9719aed
vishangi <vishangid@gmail.com>
2022-07-02 15:10:53 +0530

    Added : Given a message to analyse and respose mood

--- main/java/com/bridgelabz/MoodAnalyser.java
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -2,7 +2,6 @@ package com.bridgelabz;
 
 public class MoodAnalyser {
     String mood;
-
     public String analysisMood(String message) {
         if (message.equalsIgnoreCase("I am in sad Mood"))
             mood =  "SAD";
