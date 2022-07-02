ed302b363bc703a5fb5782ba67395c1b325777bb
vishangi <vishangid@gmail.com>
2022-07-02 15:47:37 +0530

    Added : Refactor the code to take message in constructor

--- main/java/com/bridgelabz/MoodAnalyser.java
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+    //Declare variable
     String mood;
+    String message;
+
+    //Default constructor
+    public MoodAnalyser(){
+
+    }
+
+    //Parameterised constructor
+    public MoodAnalyser(String message){
+    this.message = message;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Problem");
+    }
 
     public String analysisMood(String message) {
         if (message.equalsIgnoreCase("I am in sad Mood")) {
